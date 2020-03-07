b5403a98a8414d05ac81809a471c437ad8e70cb8
changyeon2 <47654007+changyeon2@users.noreply.github.com>
2020-03-08 05:52:56 +0900

    Create solution 1.py

---
+++ acmicpc/python3/11563/solution 1.py
@@ -0,0 +1,106 @@
+# 시간 초과 뜸 (아이디어 자체는 맞음 - 직접 교점 구해서 따져보는 방식)
+def getLine(start, end):
+    startPt = start
+    endPt = end
+    if start[0] - end[0] == 0:
+        grad = float("inf")
+        constant = 0
+    else :
+        if start[1] - end[1] == 0:
+            grad = 0
+            constant = start[0]
+        else:
+            grad = (start[1] - end[1]) / (start[0] - end[0])
+            constant = start[1] - grad * start[0]
+
+    return [grad, startPt, endPt, constant]
+
+def isOntheLine(line, point):
+    if line[0] == float("inf"):
+        if line[1][1] > line[2][1]:
+            if point[1] <= line[1][1] and point[1] >= line[2][1]:
+                return True
+            else:
+                return False
+        else:
+            if point[1] >= line[1][1] and point[1] <= line[2][1]:
+               return True
+            else:
+                return False
+    elif line[0] == 0:
+        if line[1][0] > line[2][0]:
+            if point[0] <= line[1][0] and point[0] >= line[2][0]:
+                return True
+            else:
+                return False
+        else:
+            if point[0] >= line[1][0] and point[0] <= line[2][0]:
+               return True
+            else:
+                return False
+    else:
+        verticalGrad = (-1) * 1 / line[0]
+        constant = point[1] - verticalGrad * point[0]
+
+        intersactXcor = (constant - line[3]) / (line[0] - verticalGrad)
+
+        if line[1][0] > line[2][0]:
+            if (intersactXcor <= line[1][0] and intersactXcor >= line[2][0]):
+                return True
+            else:
+                return False
+        else:
+            if (intersactXcor >= line[1][0] and intersactXcor <= line[2][0]):
+                return True
+            else:
+                return False
+
+def getDistanceBetweenPointAndLine(line, point):
+    if line[0] == float("inf"):
+        return abs(line[1][0] - point[0])
+    elif line[0] == 0:
+        return abs(line[1][1] - point[1])
+    else:
+        return (abs(line[0]*point[0] - point[1] + line[3]) / (line[0]**2 + 1)**(0.5))
+
+def getDistanceBetweenPoints(point1, point2):
+    return ((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)**(0.5)
+
+numOfSinchon, numOfAnam = map(int, input().split())
+
+sinchonRoad = dict()
+anamRoad = dict()
+
+closeDistance = []
+
+# i += 1과 같이 for문의 iterator를 바꾸는 것은 이 for문 돌아가는 것에 아무 영향을 안 줌!!
+for i in range(0, numOfSinchon):
+    temp1 = [float(x) for x in input().split()]
+    sinchonRoad[i] = getLine((temp1[0], temp1[1]), (temp1[2], temp1[3]))   
+
+for i in range(0, numOfAnam):
+    temp2 = [float(x) for x in input().split()]
+    anamRoad[i] = getLine((temp2[0], temp2[1]), (temp2[2], temp2[3]))   
+
+for i in range(0, numOfSinchon):
+    for j in range(0, numOfAnam):
+        closeDistance += [min(getDistanceBetweenPoints(sinchonRoad[i][1], anamRoad[j][1])\
+            , getDistanceBetweenPoints(sinchonRoad[i][1], anamRoad[j][2]) \
+            , getDistanceBetweenPoints(sinchonRoad[i][2], anamRoad[j][1]) \
+            , getDistanceBetweenPoints(sinchonRoad[i][2], anamRoad[j][2]))]
+
+for i in range(0, numOfSinchon): 
+    for j in range(0, numOfAnam):
+        if isOntheLine(anamRoad[j], sinchonRoad[i][1]):
+            closeDistance += [getDistanceBetweenPointAndLine(anamRoad[j], sinchonRoad[i][1])]
+        if isOntheLine(anamRoad[j], sinchonRoad[i][2]):
+            closeDistance += [getDistanceBetweenPointAndLine(anamRoad[j], sinchonRoad[i][2])]
+
+for i in range(0, numOfAnam): 
+    for j in range(0, numOfSinchon):
+        if isOntheLine(sinchonRoad[j], anamRoad[i][1]):
+            closeDistance += [getDistanceBetweenPointAndLine(sinchonRoad[j], anamRoad[i][1])]
+        if isOntheLine(sinchonRoad[j], anamRoad[i][2]):
+            closeDistance += [getDistanceBetweenPointAndLine(sinchonRoad[j], anamRoad[i][2])]
+
+print(min(closeDistance))
