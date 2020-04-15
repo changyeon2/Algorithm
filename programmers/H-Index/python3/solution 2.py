0cb3f66205b178f84ecc72849933b2220531da5a
changyeon2 <47654007+changyeon2@users.noreply.github.com>
2020-04-16 02:41:07 +0900

    Create solution 2.py

---
+++ programmers/H-Index/python3/solution 2.py
@@ -0,0 +1,14 @@
+def solution(citations):
+    citations.sort()
+    
+    length = len(citations)
+    
+    # h-index의 정의 이용!! 큰 것들의 개수가 h-index의 후보가 됨(0 ~ length개까지)
+    # 여기서는 1 ~ length개인경우 체크(0인 경우는 IndexError 뜸(out of range))
+    for i in range(length):
+        if citations[i] >= length - i:
+            return length - i
+    
+    # 여기서 h-index 0인 
+    return 0
+    
