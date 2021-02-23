4611785136fdbc0ef72f9792082db45289c06bea
Changyeon Lee <nick000219@naver.com>
2021-02-24 00:02:40 +0900

    Create solution 1.cpp

---
+++ programmers/체육복/C++/solution 1.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 그냥 reserve 입장에서 체육복 없는 애들 최대한 안 겹치게 주면 된다.
+// 단지 누구를 줄지 체크하는 것을 -1인 애들 기준으로 할 뿐!
+// loop 돌면서 앞에서부터 체육복 없는 애들 하나씩 메꿔나간다고 생각하자.
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+	
+	vector<int> students = vector<int>(n, 0);
+	
+	for(int lostIndex: lost) students[lostIndex-1] -= 1;
+	
+	for(int reserveIndex: reserve) students[reserveIndex-1] += 1;
+	
+	for(int i=0; i<n; i++){
+		if(students[i] == -1){
+			if(i != (n-1) && students[i+1] == 1){
+				students[i] = 0;
+				students[i+1] = 0;
+			}
+			else if(i != 0 && students[i-1] == 1){
+				students[i] = 0;
+				students[i-1] = 0;
+			}
+		}
+	}
+	
+	for(int student: students){
+		if(student == 0 || student == 1) answer++;
+	}
+	
+    return answer;
+}
