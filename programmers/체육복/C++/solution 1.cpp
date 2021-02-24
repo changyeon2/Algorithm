f2357b2d660d74b2e1150a9928c22a59e0463c9b
Changyeon Lee <nick000219@naver.com>
2021-02-24 22:14:11 +0900

    Update solution 1.cpp

--- programmers/체육복/C++/solution 1.cpp
+++ programmers/체육복/C++/solution 1.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-// 그냥 reserve 입장에서 체육복 없는 애들 최대한 안 겹치게 주면 된다.
-// 단지 누구를 줄지 체크하는 것을 -1인 애들 기준으로 할 뿐!
-// loop 돌면서 앞에서부터 체육복 없는 애들 하나씩 메꿔나간다고 생각하자.
+// 그냥 reserve 입장에서 체육복 없는 lost 애들 최대한 안 겹치게 주면 된다.
+// 단지 누구를 줄지 체크하는 것을 lost 기준으로 할 뿐!
+// loop를 돌면서, reserve가 lost에게 우선적으로 <-으로 주도록 하고, 그 다음으로 -> 방향을 고려한다.
 int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = 0;
 	
