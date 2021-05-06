30276a00752d73d21de4777c5a40090532f32bb5
Changyeon Lee <nick000219@naver.com>
2021-05-06 23:22:21 +0900

    Create union_find.cpp

---
+++ code_snippets/union_find/C++/union_find.cpp
@@ -0,0 +1,23 @@
+// Find function
+int findFunction(int node, int parent[]){
+	if(parent[node] == node) return node; 
+	 
+	return parent[node] = findFunction(parent[node], parent); 
+}
+
+// union function
+int unionFunction(int node1, int node2, int parent[]){
+	node1 = findFunction(node1, parent); 
+	node2 = findFunction(node2, parent); 
+
+	if(node1 < node2){
+		parent[node2] = node1;
+		
+		return node1;
+	}
+	else{
+		parent[node1] = node2;
+		
+		return node2;
+	}
+}	
