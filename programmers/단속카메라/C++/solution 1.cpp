7ec8bbc17a5421bb824828bb2e463cbe8fd9422f
Changyeon Lee <nick000219@naver.com>
2021-05-09 15:11:21 +0900

    Create solution 1.cpp

---
+++ programmers/단속카메라/C++/solution 1.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+int solution(vector<vector<int>> routes) {
+    int answer = 0;
+	
+	sort(routes.begin(), routes.end(), [](auto vec1, auto vec2){
+		return (vec1[0] > vec2[0]);
+	});
+		
+	stack<vector<int>> s;
+	
+	for(vector<int> route : routes) s.push(route);
+	
+	vector<int> popedVector;
+	vector<int> topOfStack;
+	
+	while(true){
+		if(s.size() == 1){
+			answer++;
+			s.pop();
+			break;
+		}
+		
+		popedVector = s.top();
+		s.pop();
+		topOfStack = s.top();
+		
+		if(popedVector[1] >= topOfStack[0] && popedVector[1] <= topOfStack[1]){
+			topOfStack[1] = popedVector[1];
+			s.pop();
+			s.push(topOfStack);
+		}
+        
+		else if(popedVector[0] <= topOfStack[0] && topOfStack[1] <= popedVector[1]) continue;
+		
+        else answer++;
+	}
+	
+    return answer;
+}
