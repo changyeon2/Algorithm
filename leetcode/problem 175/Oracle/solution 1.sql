1b47608b5c263fabf54dde55f66cf377d6e0d68d
Changyeon Lee <nick000219@naver.com>
2021-05-29 00:28:55 +0900

    Update solution 1.sql

--- leetcode/problem 175/Oracle/solution 1.sql
+++ leetcode/problem 175/Oracle/solution 1.sql
@@ -1,3 +1,8 @@
-SELECT FirstName, LastName, City, State
-FROM Person
-LEFT JOIN Address ON person.PersonId = Address.PersonId;
+SELECT 
+    FirstName, LastName, City, State
+FROM 
+(
+    Person
+        LEFT JOIN 
+    Address ON person.PersonId = Address.PersonId;
+)
