526767ae3047d1e0b092941d0421a83edbf1193b
Changyeon Lee <nick000219@naver.com>
2021-06-02 23:50:01 +0900

    Create solution 1.sql

---
+++ leetcode/problem 626/Oracle/solution 1.sql
@@ -0,0 +1,16 @@
+-- Using CASE WHEN ~ THEN ~ ELSE ~ END
+
+SELECT
+    id AS "id",
+    CASE WHEN MOD(id, 2) = 0
+    THEN LAG(student) OVER(ORDER BY id)
+    ELSE(
+        CASE WHEN LEAD(student) OVER(ORDER BY id) IS NULL
+        THEN student
+        ELSE LEAD(student) OVER(ORDER BY id)
+        END
+        )
+    END AS "student"
+FROM
+    seat
+ORDER BY "id";
